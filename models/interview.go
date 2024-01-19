23a0119078ccbf2238405664ab77a77b7747ad6f
Arpit Chinmay <theinvinciblearpitchinmay@gmail.com>
2024-01-19 10:30:20 +0530

    SQLite DB integration into existing code.

--- models/interview.go
+++ models/interview.go
@@ -1,11 +1,11 @@
 package models
 
-// type Interview struct {
-// 	CandidateId    int  `json:"candidateId"`
-// 	ResumeId       int  `json:"resumeId"`
-// 	LevelOne       bool `json:"levelOne"`
-// 	LevelTwo       bool `json:"levelTwo"`
-// 	Managerial     bool `json:"managerial"`
-// 	OfferRolledOut bool `json:"offerRolledOut"`
-// 	OfferStatus    int  `json:"offerStatus"`
-// }
+type Interview struct {
+	CandidateId    string `json:"candidateId"`
+	ResumeId       string `json:"resumeId"`
+	LevelOne       int    `json:"levelOne"`
+	LevelTwo       int    `json:"levelTwo"`
+	Managerial     int    `json:"managerial"`
+	OfferRolledOut int    `json:"offerRolledOut"`
+	OfferStatus    int    `json:"offerStatus"`
+}
